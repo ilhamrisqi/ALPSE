9d32943226ecc3e24562e8a054a2c6cfaf03f8d9
ilhamrisqi <92148866+ilhamrisqi@users.noreply.github.com>
2023-05-15 08:43:34 +0700

    Initial Commit

---
+++ ALPSE/ALPSEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALPSEApp.swift
+//  ALPSE
+//
+//  Created by ilham risqi on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ALPSEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
